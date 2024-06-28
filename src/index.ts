652a173c708eec2daa13e0fa55ba9f68e26a0647
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-28 23:15:47 +0600

    Any type

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,22 @@
-// Functions
+// Any type
+let age: any;
+age = 12;
+age = "hello";
+age = true;
 
-// Parameter types
-function addTwoNumbers(a: number, b: number) {
-  return a + b;
+let title;
+title = "hello";
+title = 25;
+title = true;
+
+// Any type in arrays
+let names: any[] = ["Pete", "Bob", true];
+names.push(12);
+
+// Any type in functions
+function addTogether(value: any): any {
+  return value + value;
 }
 
-console.log(addTwoNumbers(2, 2));
-
-// error
-// console.log(addTwoNumbers(2, "2"));
-
-// Return types
-function subtractTwoNumbers(a: number, b: number): number {
-  return a - b;
-}
-
-console.log(subtractTwoNumbers(20, 2));
-
-// return void
-function addAllNumbers(items: number[]): void {
-  console.log(items.reduce((a, b) => a + b, 0));
-}
-
-addAllNumbers([1, 2, 3, 4, 5]);
-
-// error
-// console.log(addAllNumbers([1, 2, 3, 4, "5"]));
-
-// return type inference
-function formatGreeting(name: string, greeting: string) {
-  return `${greeting}, ${name}!`;
-}
-
-const result = formatGreeting("Pete", "Hello");
+console.log(addTogether("hello"));
+console.log(addTogether("5"));
