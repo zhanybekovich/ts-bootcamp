c70d5daf360389d646a2b1d418e7f07bcc345adb
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-28 22:19:38 +0600

    Basic type annotating

--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
-let age = 30;
-console.log(age);
+let userAge: number = 30;
+let firstName: string = "John";
+let isFictional: boolean;
+
+// errors
+// userAge = '30';
+// firstName = true;
+
+// valid
+isFictional = false;
+
+// type inference
+let planet = "Earth";
+let moons = 1;
+let isLarge = false;
+
+// errors
+// planet = 1;
+// moons = '1';
+// isLarge = 'false';
