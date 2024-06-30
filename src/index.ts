e9410d89410644aa9d756b4fb562a7afbb6c7dfc
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-30 20:15:51 +0600

    Intersection types

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,23 @@
-// Generics
+// Intersection types
 
-function logAndReturnString(val: string): string {
-  console.log(val);
-  return val;
+interface HasID {
+  id: number;
 }
 
-function logAndReturnNumber(val: number): number {
-  console.log(val);
-  return val;
+function addIdToValue<T>(val: T): T & HasID {
+  const id = Math.random();
+
+  return { ...val, id };
 }
 
-function logAndReturnBoolean(val: boolean): boolean {
-  console.log(val);
-  return val;
+interface Post {
+  title: string;
+  thumbsUp: number;
 }
 
-// using generics
+const post = addIdToValue<Post>({
+  title: "Hello",
+  thumbsUp: 0,
+});
 
-function logAndReturnValue<T>(val: T): T {
-  console.log(val);
-  return val;
-}
-
-const result1 = logAndReturnValue<string>("hello");
-const result2 = logAndReturnValue<number>(10);
-const result3 = logAndReturnValue<boolean>(true);
-
-function getRandomArrayValue<T>(values: T[]): T {
-  const i = Math.floor(Math.random() * values.length);
-
-  return values[i];
-}
-
-interface User {
-  name: string;
-  score: number;
-}
-
-const users: User[] = [
-  {
-    name: "John",
-    score: 10,
-  },
-  {
-    name: "Jane",
-    score: 20,
-  },
-  {
-    name: "Bob",
-    score: 40,
-  },
-  {
-    name: "Mike",
-    score: 400,
-  },
-];
-
-const randomUser = getRandomArrayValue<User>(users);
-console.log(randomUser);
+console.log(post);
